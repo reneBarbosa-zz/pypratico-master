8fe4fc775b1d6e9c9915fc210a2953668cadc058
reneBarbosa <renefabarbosa@gmail.com>
2018-03-07 15:39:50 -0300

    test fix rediscar (comentado  anagrama)ffrfrfg
    fixes #5

--- tests/test_anagrama.py
+++ tests/test_anagrama.py
@@ -2,4 +2,4 @@ from exercicios.anagrama import eh_anagrama
 
 
 # def test_anagrama_simples():
-#     assert eh_anagrama('the alias men', 'alan smithee')
+#     assert eh_anagrama('the alias men', 'alan smithee')iiyyyyyyyyy
