2fe66ab1b215ba5eb8985a6f13f2a82084c8fa69
reneBarbosa <renefabarbosa@gmail.com>
2018-03-07 14:33:07 -0300

    test rediscar

--- pypraticot6/telefone.py
+++ pypraticot6/telefone.py
@@ -1,3 +1,11 @@
+
 class Telefone(object):
+    def __init__(self):
+        self.ultimo_numero = None
+
     def ligar(self, numero):
+        self.ultimo_numero = numero
         return f'ligar para {numero}'
+
+    def rediscar(self):
+        return self.ligar(self.ultimo_numero)
--- tests/test_telefone.py
+++ tests/test_telefone.py
@@ -2,7 +2,7 @@ import pytest
 
 from pypraticot6.telefone import Telefone
 
-NUMEROS_VALIDOS = ['1258446', 26264466]
+NUMEROS_VALIDOS = ['1258446', 2345678]
 
 def test_telefone_init():
     assert (Telefone() is not None)
@@ -11,3 +11,18 @@ def test_telefone_init():
 def test_ligar(numero):
     telefone = Telefone()
     assert 'ligar para ' + str(numero) == telefone.ligar(numero)
+
+@pytest.mark.parametrize('numero', NUMEROS_VALIDOS)
+def test_rediscar():
+    telefone = Telefone()
+    numero_anterior = '2345678'
+    telefone.ligar(numero_anterior)
+    assert 'ligar para ' + str(numero_anterior) == telefone.rediscar()
+
+def test_rediscar_dois_telefones_diferentes(numero):
+    telefone = Telefone()
+    telefone.ligar(numero)
+    telefone2 = Telefone()
+    outro_numero = str(numero) + '1'
+    telefone2.ligar(outro_numero)
+    assert 'ligar para ' + str(outro_numero) == telefone.rediscar()
